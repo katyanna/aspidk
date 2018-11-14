7cce326092b22556da75f81ff707b5d46400b75d
Katyanna Moura <amelie.kn@gmail.com>
2018-11-14 14:34:45 +0100

    Add Cards view

--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Concept
+import random
+import pdb
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the cards index.")
+    min = Concept.objects.first().id
+    max = Concept.objects.last().id
+    rand_id = random.randint(min, max)
+    concept = Concept.objects.get(id=rand_id)
+    context = {'concept': concept,}
+    return render(request, 'cards/index.html', context)
